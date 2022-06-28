8e3b422bf49edeef2632a7a48adf3c99e5ed9efa
meghaksh4footprints@gmail.com <meghaksh4footprints@gmail.com>
2022-06-28 11:50:19 -0400

    changes made with useEffect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,34 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    console.log("Type 1");
+  }, [enteredEmail]);
+
+  /**The setFormIsValid function will run every 500 ms.
+   * Here we have clean function which clear the timeout.
+   * Cleanup function runs before every execution of the useEffect,
+   * except the first execution.
+   */
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("checking form validity");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+    return () => {
+      console.log("Cleanup");
+      clearTimeout(identifier);
+    };
+    /**Below lines are executed on every keystroke.
+     * We want to make sure that these are executed on every 500 ms.
+     * For that we have used them inside a timer.
+     */
+    // console.log("checking form validity");
+    // setFormIsValid(
+    //   enteredEmail.includes("@") && enteredPassword.trim().length > 6
+    // );
+  }, [enteredEmail, enteredPassword]);
+
+  /**Here email change handler will check on the every keystroke if the form is valid.
+   * Therefor, setForIsValid is a side effect of the email value change in the input field.
+   * We remove it from below and add as a side effect with dependency on enteredEmail and password.
+   */
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes("@") && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes("@")
+    // );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -45,7 +78,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +92,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
